bf18d7f261af348a89b2808f25455d4b0956ef7b
SahilBhagchandani <sahilbhagchandani98@gmail.com>
2021-04-25 20:37:36 -0700

    commit

--- Frontend/src/webConfig.js
+++ Frontend/src/webConfig.js
@@ -1,2 +1,3 @@
-const backendServer = "http://localhost:3001/api";
+// const backendServer = "http://localhost:3001/api";
+const backendServer = "http://18.116.112.11:3001/api";
 export default backendServer;
